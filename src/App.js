27f5427a2100f79959cc2755a941c848faca426b
alkeops <m_adan2012@outlook.com>
2021-10-26 20:22:09 -0500

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavBar } from "components/organism"; //Para este tipo de importaciones ver el archivo jsconfig.json
 
+//La importación de react ya no es necesaria desde la version 17 pero se puede poner si asi lo quieren.
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/organism/index.js
@@ -0,0 +1,6 @@
+import NavBar from "./NavBar/NavBar";
+
+export { NavBar };
+
+//Esto lo hago para poder realizarlas importaciones desde un unico sitio, por ejemplo from "componentes/NavBar"
+//Ver jsconfig.json
