aff3bd1d524af482d5cf8940ad69e8164fe982c5
alkeops <m_adan2012@outlook.com>
2021-10-29 20:58:10 -0500

    Div

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import Home from "components/views/Home/Home";
 //La importación de react ya no es necesaria desde la version 17 pero se puede poner si asi lo quieren.
 function App() {
   return (
-    <>
+    <div className="App">
       <Home />
-    </>
+    </div>
   );
 }
 
